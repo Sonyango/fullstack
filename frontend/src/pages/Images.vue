b9dd1b0bb09b6c0168dd5fb1e41c1cda7d1f20e7
Sonyango <stephenonyango82@gmail.com>
2025-06-23 22:55:38 +0300

    Image editing functionality.

--- frontend/src/pages/Images.vue
+++ frontend/src/pages/Images.vue
@@ -2,16 +2,14 @@
 import { PhotoIcon } from '@heroicons/vue/24/solid';
 import { onMounted, ref } from 'vue';
 import axiosClient from '../axios';
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue';
+import { ExclamationTriangleIcon } from '@heroicons/vue/24/outline';
 
-const showModal = ref(false);
+const open = ref(false);
+const selectedImage = ref(null);
+const newImageFile = ref(null);
+const newImagePreviewUrl = ref(null);
 
-function openModal() {
-    showModal.value = true;
-}
-
-function closeModal() {
-    showModal.value = false;
-}
 
 const images = ref([]);
 
@@ -28,6 +26,17 @@ function deleteImage(id) {
         .then(response => {
             images.value = images.value.filter(image => image.id !== id);
         })
+}
+
+function handleEditFileChange(event) {
+    const file = event.target.files[0];
+    newImageFile.value = file;
+
+    if (file) {
+        newImagePreviewUrl.value = URL.createObjectURL(file);
+    } else {
+        newImagePreviewUrl.value = null;
+    }
 }
 
 onMounted(() => {
@@ -56,7 +65,7 @@ onMounted(() => {
                         <h3 class="text-lg font-semibold text-gray-900">{{ image.name }}</h3>
                         <p class="text-sm text-gray-500 mb-4">{{ image.label }}</p>
                         <div class="flex justify-between">
-                            <button type="submit" @click="openModal"
+                            <button type="submit" @click="open = true; selectedImage = image"
                                 class="rounded-md bg-green-600 px-3 py-1 text-sm/6 font-semibold text-white shadow-sm hover:bg-green-700 focus-visible:outline">
                                 Edit
                             </button>
@@ -69,31 +78,75 @@ onMounted(() => {
                                 Delete
                             </button>
 
-                            <div v-if="showModal"
-                                class="fixed inset-0 z-50 flex items-center justify-center bg-white-500 bg-opacity-50">
-                                <div class="bg-white rounded-lg shadow-xl w-full max-w-md">
-                                    <div class="px-6 py-4">
-                                        <!-- <h3 class="text-lg font-medium text-gray-900">Edit Image</h3> -->
-                                        <p class="mt-2 text-sm text-gray-600">
-                                            <PhotoIcon class="mx-auto size-12 text-gray-300" aria-hidden="true" />
-                                        </p>
-                                    </div>
-                                    <div class="bg-gray-100 px-6 py-3 flex justify-center rounded-b-lg">
-                                        <button @click="closeModal"
-                                            class="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 mr-2">
-                                            Cancel
-                                        </button>
-                                        <button @click="closeModal"
-                                            class="rounded-md bg-indigo-600 px-3 py-1 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                            Save
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-
                         </div>
                     </div>
-                </div>
+                </div>                              
+
+                    <TransitionRoot as="template" :show="open">
+                    <Dialog class="relative z-10" @close="open = false">
+                        <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+                        <div class="fixed inset-0 bg-gray-500/75 transition-opacity" />
+                        </TransitionChild>
+
+                        <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                            <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                <div class="sm:flex sm:items-start">
+                                    <div class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:size-10">
+                                    <ExclamationTriangleIcon class="size-6 text-red-600" aria-hidden="true" />
+                                    </div>
+                                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                    <DialogTitle as="h3" class="text-base font-semibold text-gray-900">Update Existing Image</DialogTitle>
+
+                                    <div class="mt-2 flex justify-between">
+                                        <div class="rounded-lg border border-dashed border-gray-900/25 px-6 py-10 mr-2">
+                                            <p>Existing image</p>
+                                            <img :src="selectedImage.url" :alt="selectedImage.name" class="mt-2 max-h-32 max-w-full object-contain" />
+                                        </div>
+
+                                        <div class="rounded-lg border border-dashed border-gray-900/25 px-6 py-10 ml-2">
+                                            <p v-if="newImagePreviewUrl">Selected image</p>
+                                            <template v-if="!newImagePreviewUrl">
+                                                
+                                                 <label for="file-upload"
+                                                class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:outline-hidden hover:text-indigo-500">
+                                                <span>Upload file</span>
+                                                <input id="file-upload" name="file-upload" type="file" @change="handleEditFileChange" class="sr-only" /> 
+                                            </label>
+                                            </template>
+                                            <template v-else>
+                                                <img :src="newImagePreviewUrl" alt="New Image Prevew" class="mt-2 max-h-32 max-w-full object-contain rounded-md" />
+                                                <div class="text-sm text-gray-600">{{ newImageFile.name }}</div>
+                                            </template>
+                                            
+                                           
+                                        </div>
+                                        
+                                    </div>
+                                    
+
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                <button type="button" 
+                                    class="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-green-500 sm:ml-3 sm:w-auto"
+                                    :disabled="!newImageFile"
+                                    :class="{ 'opacity-40 cursor-not-allowed': !newImageFile }" 
+                                    @click="open = false">
+                                    Update Image
+                                </button>
+                                <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="open = false" ref="cancelButtonRef">Cancel</button>
+                                </div>
+                            </DialogPanel>
+                            </TransitionChild>
+                        </div>
+                        </div>
+                    </Dialog>
+                    </TransitionRoot>
+                            
             </div>
         </div>
     </main>
